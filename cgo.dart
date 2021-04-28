5c8c7669c01f8f877d7a5f0dff4cd569f30ac1af
Coding Pepper <83090340+codingpepper@users.noreply.github.com>
2021-04-28 12:28:16 +0200

    Update cgo.dart

--- cgo.dart
+++ cgo.dart
@@ -1,90 +1,145 @@
+import 'dart:io';
+
 void main() {
-  
- 
-  final List<Player> Players = [];
-  
-  final PlayerOne = Player([], 'Hans', false);
-  final PlayerTwo = Player([], 'Werner', true); 
-  final PlayerThree = Player([], 'Achim', false); 
-  final PlayerFour =  Player([], 'Johannes', false); 
- 
+  List<Player> Players = [];
+
+  // Making Maps out of it because of the key features
+  var PlayerOne = Player([], 'Hans');
+  var PlayerTwo = Player([], 'Werner');
+  var PlayerThree = Player([], 'Achim');
+  var PlayerFour = Player([], 'Johannes');
+
+  bool weArePlayingSolo = false;
+
   Players.add(PlayerOne);
   Players.add(PlayerTwo);
   Players.add(PlayerThree);
   Players.add(PlayerFour);
-  
+
   var deck = new Deck();
- 
+
   deck.shuffle();
-  
-  
+  print(deck.cards[0].cardScore);
 
-  var gameIndex = 0; 
-  
+  var tableCards = [];
 
-  
-  for(var x = 0; x < deck.cards.length; x++){
-      if(x < 13){
-        PlayerOne.playerCards.add(deck.cards[x]);
-      } else if(x < 26) {
-        PlayerTwo.playerCards.add(deck.cards[x]);
-      } else if(x > 26 && x <= 39){
-        PlayerThree.playerCards.add(deck.cards[x]);
-      } else {
-        PlayerFour.playerCards.add(deck.cards[x]);
-      }
+  var gameIndex = 0;
+
+  // Distribute Cards To Player
+  for (var x = 0; x < deck.cards.length; x++) {
+    if (x < 13) {
+      PlayerOne.playerCards.add(deck.cards[x]);
+    } else if (x < 26) {
+      PlayerTwo.playerCards.add(deck.cards[x]);
+    } else if (x > 26 && x <= 39) {
+      PlayerThree.playerCards.add(deck.cards[x]);
+    } else {
+      PlayerFour.playerCards.add(deck.cards[x]);
+    }
   }
-  
+
   var myGame = Game(Players);
+
+  //var myTableCards = CardsOnTheTable();
+
   var myOponentsArray = [];
 
-  
-  for(var i = 0; i < 4; i++){
-    if(myGame.checkForSolo(i).isPlayingSolo){
-      print('Hellow');
-    }; 
+  for (var i = 0; i < Players.length; i++) {
+    myGame.checkForSolo(i);
+    if (Players[i]._isPlayingSolo == true) {
+      print('${Players[i].name} is Playing a Solo');
+      var mySolo = Solo(Players[i], Players);
+      print('The solist is');
+      print(mySolo.theSolist.name);
+      weArePlayingSolo = true;
+    }
+    print(Players[i]._isPlayingSolo == true);
   }
-  
-  print(myOponentsArray[0]);
-  
 
+  if (weArePlayingSolo) {
+    for (var i = 0; i < 2; i++) {
+      print('${i} RoundCounter');
+      for (var x = 0; x < 4; x++) {
+        // stdout.write("${Players[x].name} Which Card you want to take ? ");
+        // indexToRemove = stdin.readLineSync();
+        // Create Map from Players and their cards thrown on the table for Every Round
+        var myLast = {
+          'PlayerCards': Players[x].playerCards.last,
+          'Player': Players[x]
+        };
+        tableCards.add(myLast);
+        Players[x].playerCards.removeLast();
+        //print(tableCards);
+        //print(myTableCards._tableCards[0]);
+        //print( 'Player Number Cards ${Players[x].name} ${Players[x].playerCards} ${Players[x].playerCards.length}');
+      }
+      // Check for highest Card on the table and which player is owning it
+      var cardCheckerVar = 0;
+      var roundWinnerVar = null;
 
-  
+      for (var c = 0; c <= 3; c++) {
+        if (tableCards[c]['PlayerCards'].cardScore > cardCheckerVar) {
+          cardCheckerVar = tableCards[c]['PlayerCards'].cardScore;
+          print('${cardCheckerVar} + CardCeckerVar');
+          roundWinnerVar = tableCards[c]['Player'].name;
+          //print("Winner is ${roundWinnerVar}");
+        }
+        print(
+            '${tableCards[c]['PlayerCards'].cardScore} + ${tableCards[c]['Player'].name}');
+      }
+      print('${roundWinnerVar} is the Winner of the Round');
+    }
+  }
 
-  
-  
-  
-  
-// print(deck.cardsWithSuit('Diamonds')); //Whatever variable is passed to print(), print() looks for a corresponding toString()
+  //print(tableCards[0]['Player'].name);
+
+  //print(deck.cardsWithSuit('Diamonds')); //Whatever variable is passed to print(), print() looks for a corresponding toString()
   //of that variable and then executes it. So, if we define toString() for Deck class, we can emulate
   //any function we want
 }
 
 class Deck {
-  List<Card> cards = []; //List creation and initialization. If not initialized, it is null
+  List<Card> cards =
+      []; //List creation and initialization. If not initialized, it is null
   //List is same as array in JAVA. So, saying List<Card> cards essentially means an array of
   //Card objects with the array variable name as cards
   Deck() {
-    var ranks = ['Ace', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King'];
+    var ranks = [
+      'Ace',
+      'Two',
+      'Three',
+      'Four',
+      'Five',
+      'Six',
+      'Seven',
+      'Eight',
+      'Nine',
+      'Ten',
+      'Jack',
+      'Queen',
+      'King'
+    ];
     var suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
-    
-    for(var suit in suits) {
-      for(var rank in ranks) {
-        var card = new Card(rank, suit);
-        cards.add(card); //If list not initialized, list becomes null and add() will not work
+    var cardScores = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+
+    for (var suit in suits) {
+      for (var i = 0; i < ranks.length; i++) {
+        var card = new Card(ranks[i], suit, cardScores[i]);
+        cards.add(
+            card); //If list not initialized, list becomes null and add() will not work
       }
     }
   }
-  
+
   toString() {
     return cards.toString();
   }
-  
+
   //Shuffles all cards of the deck randomly
   shuffle() {
     cards.shuffle();
   }
-  
+
   //Short form of function syntax. Long form below
   //Returns all cards of a specific suit
   cardsWithSuit(String suit) {
@@ -92,9 +147,9 @@ class Deck {
     //Whatever is evaluated on the right is automatically returned to the left
     return cards.where((card) => card.suit == suit);
     //Long form of function syntax
-    //return cards.where((card) { 
-      //where() needs a method as param, so passed a nameless method which returns a bool value
-      //return card.suit == suit;
+    //return cards.where((card) {
+    //where() needs a method as param, so passed a nameless method which returns a bool value
+    //return card.suit == suit;
     //});
   }
 }
@@ -102,68 +157,72 @@ class Deck {
 class Card {
   String rank;
   String suit;
-  
-  Card(this.rank, this.suit);
-  
+  int cardScore;
+
+  Card(this.rank, this.suit, this.cardScore);
+
   toString() {
     return '$rank of $suit';
   }
 }
 
-
 class Player {
-  final String name;
-  final List<Card> playerCards;
-  final bool isPlayingSolo;
-  
-  Player(this.playerCards, this.name, this.isPlayingSolo);
-  
-  void printIT(){
+  var name;
+  var playerCards;
+  bool _isPlayingSolo = false;
+
+  Player(this.playerCards, this.name);
+
+  set isPlayingSolo(bool isPlayingSolo) {
+    this._isPlayingSolo = isPlayingSolo;
+  }
+
+  void printIT() {
     print('Card');
   }
-  
-  
 }
 
-
-
-
-
-class Game{
+class Game {
   final List<Player> thePlayers;
-  
+  bool _activePlayer = false;
+
   Game(this.thePlayers);
-  
-  Player checkForSolo(int iterator){
-      if(thePlayers[iterator].isPlayingSolo == false){
-        //print('${thePlayers[iterator].name} is not Playing a solo')
-        print(thePlayers[iterator].name);
-        return thePlayers[iterator];
-      } else {
-        print('${thePlayers[iterator].name} is Playing a solo');
-        return thePlayers[iterator];
-      }
-  }
-  
-  bool checkForCego(){
-    return true; 
+
+  void checkForSolo(int iterator) {
+    stdout.write("${thePlayers[iterator].name} you want to play solo ? ");
+    var myBool = stdin.readLineSync();
+    if (myBool == 'Yes') {
+      print('Setting to True');
+      thePlayers[iterator].isPlayingSolo = true;
+    }
   }
 
-  
+  set activePlayer(bool activePlayer) {
+    this._activePlayer = activePlayer;
+  }
+
+  bool checkForCego() {
+    return true;
+  }
 }
 
-
-class Solo{
+class Solo {
   final Player theSolist;
-  final List<Player> theOpponents; 
-  
+  List<Player> theOpponents;
+
   Solo(this.theSolist, this.theOpponents);
-  
 }
 
+// class CardsOnTheTable {
+//   var _tableCards = [];
 
+//   CardsOnTheTable();
 
+//   set tableCards(tableCards) {
+//     this._tableCards.add(tableCards);
+//   }
 
-
-
-
+//   void printTableCards() {
+//     _tableCards[0];
+//   }
+// }
